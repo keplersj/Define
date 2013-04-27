783ecb3635bf5279c6627b6611494b6ad3a5d2f4
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-04-27 14:54:04 -0600

    Created JavaScript File
    With some code copied from Google. ;)

---
+++ index.js
@@ -0,0 +1,7 @@
+var cx = '004985886206999086560:ncgdthc5tlu';
+var gcse = document.createElement('script');
+gcse.async = true;
+gcse.type = 'text/javascript';
+gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);

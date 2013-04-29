3508633c72f9a39d60126b5afc06087ce5c7b03c
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-04-28 23:35:18 -0600

    All JavaScript is being used in an external file!

--- Source Code/index.js
+++ Source Code/index.js
@@ -1,7 +1,10 @@
-var cx = '004985886206999086560:ncgdthc5tlu';
-var gcse = document.createElement('script');
-gcse.async = true;
-gcse.type = 'text/javascript';
-gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(gcse, s);
+(function() {
+  var cx = '004985886206999086560:ncgdthc5tlu';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+  '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();

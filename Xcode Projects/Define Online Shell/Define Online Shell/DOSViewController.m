0def35fba63d390b304ae5d1da0bd26cfd032ee7
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-04-30 23:41:55 -0600

    Added Blank iOS App

---
+++ Xcode Projects/Define Online Shell/Define Online Shell/DOSViewController.m
@@ -0,0 +1,29 @@
+//
+//  DOSViewController.m
+//  Define Online Shell
+//
+//  Created by Kepler Sticka-Jones on 4/30/13.
+//  Copyright (c) 2013 Project Define. All rights reserved.
+//
+
+#import "DOSViewController.h"
+
+@interface DOSViewController ()
+
+@end
+
+@implementation DOSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Xcode Projects/Define Online Shell/Define Online Shell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Define Online Shell
+//
+//  Created by Kepler Sticka-Jones on 4/30/13.
+//  Copyright (c) 2013 Project Define. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DOSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DOSAppDelegate class]));
+    }
+}

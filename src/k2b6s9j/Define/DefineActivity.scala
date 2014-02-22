036908c43d82cb4e13830d669efa11ee688ba297
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-21 23:47:21 -0700

    'Working' Setup and Hello World written in Scala.

---
+++ src/k2b6s9j/Define/DefineActivity.scala
@@ -0,0 +1,15 @@
+package k2b6s9j.Define
+
+import android.app.Activity
+import android.os.Bundle
+
+class DefineActivity extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    override def onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
